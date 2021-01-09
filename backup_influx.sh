9807bdad51dd1517db5a561aab051b90b4346d4d
IT-Buster <bartek@bk.waw.pl>
2021-01-09 21:28:47 +0100

    mqtt2influx init2

--- backup_influx.sh
+++ backup_influx.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
 echo `date`: Myscript starts
-
 cd /grafana/influxdb/backup/ && rm *.gz *.meta *.manifest
-
 docker exec influxdb influxd backup -portable /var/lib/influxdb/backup/
-
 ssh root@192.168.1.5 'cd /mnt/sda1/backup_influx/backup && rm *.manifest *.meta *.gz'
-
 scp -r /grafana/influxdb/backup root@192.168.1.5:/mnt/sda1/backup_influx
-
 echo `date`: Myscript stops
